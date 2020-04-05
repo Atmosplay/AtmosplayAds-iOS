0bab8808fc78d80e5bc4b8dd5e9c1ca1b78b2378
JiaDingYi <wzy2010416033@163.com>
2020-04-05 18:02:44 +0800

    add float

---
+++ PlayableAdsDemo/Demo/WindowAd/AtmosplayWindowAdViewController.h
@@ -0,0 +1,17 @@
+//
+//  AtmosplayWindowAdViewController.h
+//  PlayableAds_Example
+//
+//  Created by jdy on 2020/3/23.
+//  Copyright © 2020 AtmosplayAds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AtmosplayWindowAdViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
